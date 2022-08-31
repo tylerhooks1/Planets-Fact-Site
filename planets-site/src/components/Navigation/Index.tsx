35ebca01dc8ef0d57fd1e73dfd17974dff5a4350
Tyler Hooks <tylerhooks8@gmail.com>
2022-08-30 22:00:51 -0500

    Added React App, Began adding apollo for data, TS components

---
+++ planets-site/src/components/Navigation/Index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navigaton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Navigaton;
